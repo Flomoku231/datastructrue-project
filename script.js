428412aa111de1c64e7695acb60994a9fad2a990
Flomoku231 <flomokuemmanuel114@gmail.com>
2023-02-28 22:10:22 +0200

    popup

--- script.js
+++ script.js
@@ -1,48 +1,60 @@
 // JavaScript Logic
 
 // Variables
-const loginForm = document.querySelector('.login__form');
-const loginUsername = document.querySelector('.username--input');
-const loginReg = document.querySelector('.reg--input');
-const loginUsernameError = document.getElementById('login__username--error');
-const loginRegError = document.getElementById('login__reg--error');
+// const loginForm = document.querySelector('.login__form');
+// const loginUsername = document.querySelector('.username--input');
+// const loginReg = document.querySelector('.reg--input');
+// const loginUsernameError = document.getElementById('login__username--error');
+// const loginRegError = document.getElementById('login__reg--error');
 
 
-loginForm.addEventListener('submit', function(e){
-    e.preventDefault();
+// loginForm.addEventListener('submit', function(e){
+//     e.preventDefault();
 
-    if(loginUsername.value == ''){
-        loginUsernameError.innerHTML = 'Please enter username'
-    }
-    if (loginReg.value !== '12345'){
-        loginRegError.innerText = 'Invaled Reg number'
-    } 
-    else if (loginUsername.value !== 'Emmanuel'){
-        loginUsernameError.innerHTML = 'Username does not exist'
+//     if(loginUsername.value == ''){
+//         loginUsernameError.innerHTML = 'Please enter username'
+//     }
+//     if (loginReg.value !== '12345'){
+//         loginRegError.innerText = 'Invaled Reg number'
+//     } 
+//     else if (loginUsername.value !== 'Emmanuel'){
+//         loginUsernameError.innerHTML = 'Username does not exist'
 
-    }
+//     }
      
-    else if(loginReg.value == ''){
-        loginRegError.innerHTML = 'Reg number is required'
+//     else if(loginReg.value == ''){
+//         loginRegError.innerHTML = 'Reg number is required'
 
-    }
+//     }
    
     
-    else{
-        console.log('Success');
-        loginReg.value = '';
-        loginUsername.value = '';
-        loginRegError.innerHTML = '';
-        loginUsernameError.innerHTML = '';
+//     else{
+//         console.log('Success');
+//         loginReg.value = '';
+//         loginUsername.value = '';
+//         loginRegError.innerHTML = '';
+//         loginUsernameError.innerHTML = '';
 
-        Redirect();
+//         Redirect();
 
-    }
+//     }
 
    
-})
+// })
 
-const Redirect = ()=>{
-    window.location.replace('stack-queue-mis.html');
-    return false;
+// const Redirect = ()=>{
+//     window.location.replace('stack-queue-mis.html');
+//     return false;
+// }
+
+// Signup popup
+const signPopup = document.querySelector('.popup--container');
+
+const showPopup=()=>{
+    signPopup.classList.add('popup--display');
+   
+}
+const hidePopup = ()=>{
+    signPopup.classList.remove('popup--display');
+    window.location.replace('login-form.html')
 }
